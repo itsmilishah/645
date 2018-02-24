1ad4ba535b42cdc58234037098d0d4a42dc118fb
Mili Shah <13bce105@nirmauni.ac.in>
2018-02-24 01:18:21 -0500

    Q4c done

---
+++ hw_2/Q4c.sql
@@ -0,0 +1,22 @@
+with pods_pubs
+as (select pubid
+	from Publication natural join inproceedings
+	where booktitle='PODS')
+, sigmod_pubs
+as (select pubid
+	from Publication natural join inproceedings
+	where booktitle='SIGMOD Conference')
+, pods
+as (select id
+	from Authored
+	where pubid in (select pubid from pods_pubs))
+, sig
+as (select id, count(pubid) as cnt
+	from Authored
+	where pubid in (select pubid from sigmod_pubs)
+	group by id
+	having count(pubid)>=2)
+select name, cnt
+from Author natural join sig
+where id not in (select id from pods)
+order by cnt desc, name asc;
