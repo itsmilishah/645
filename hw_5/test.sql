9d17083abd411c3ca1e5bc1ca5fd5d655a5475e1
Mili Shah <13bce105@nirmauni.ac.in>
2018-04-20 22:20:50 -0400

    All HWs complete

--- hw_5/test.sql
+++ hw_5/test.sql
@@ -1,6 +1,2 @@
-select year, type, count(papers.id) as publications
-	from papers
-		join venue on papers.venue = venue.id
-	where year != 0
-	group by year, type
-	order by year, type
+CREATE FUNCTION generate_year_series()
+	RETURNS textarray AS $$ SELECT CONCAT('year_', s) FROM generate_series(1995, 2015)) $$ language sql;
